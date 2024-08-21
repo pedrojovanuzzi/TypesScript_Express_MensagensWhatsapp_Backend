6b42f5dce62a869a957f18366023a4ed373bc629
Pedro Artur <arturjovanuzzi@outlook.com>
2024-08-21 16:39:47 -0300

    Adicionando Log para erros

--- src/controllers/EmailController.ts
+++ src/controllers/EmailController.ts
@@ -19,10 +19,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// cron.schedule('*/50 * * * * *', () => {
-//     console.log('running a task every minute');
-//     emailController.DiasAntes15();
-// });
+cron.schedule('* * * * *', () => {
+    console.log('running a task every minute');
+    emailController.DiasAntes5();
+    emailController.DiasDoVencimento();
+});
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-mail.outlook.com',
@@ -42,7 +43,33 @@ const transporter = nodemailer.createTransport({
 
 class EmailController {
 
-    async DiasAntes15() {
+    msg(msg : string, formattedDate : string, login : string, linhadig : string, pix : any, endereco : any, numero : any){
+
+        msg = `<p><strong>${login.toUpperCase()}</strong></p>\n`;
+
+        if (linhadig) {
+            msg += `<p>Segue a linha digitável do seu boleto da mensalidade:</p>\n`;
+            msg += `<p><strong>${linhadig}</strong></p>\n`;
+        }
+    
+        if (pix) {
+            msg += `<p>Segue o código <strong>copia e cola</strong> do Pix:</p>\n`;
+            msg += `<p><strong>${pix}</strong></p>\n`;
+        }
+    
+        msg += `<p>DATA DE VENCIMENTO: <strong>${formattedDate}</strong></p>`;
+        msg += `<p>ENDEREÇO: ${endereco.toUpperCase()} Nº${numero}</p>`;
+        msg += `<p>BOT DE AUTO-ATENDIMENTO WIP TELECOM, disponível 24 horas, para efetuar pagamentos, super rápido e dinâmico, CLIQUE NO LINK ABAIXO:</p>`;
+        msg += `<p><a href="https://wa.me/message/MWHGELKC45WDN1">https://wa.me/message/MWHGELKC45WDN1</a></p>`;
+        msg += `<p><em>Mensagem automática, não responder neste e-mail. Para dúvidas, entre em contato pelo número (14)98233-2963</em></p>`;
+        msg += `<p>Att.: WIP TELECOM</p>`;
+        msg += `<p>Agradecemos sua preferência!</p>`;
+        msg += `<p style="color: red;"><strong>ATENÇÃO: CASO JÁ TENHA PAGO/AGENDADO, DESCONSIDERE ESTE E-MAIL!</strong></p>`;
+    
+        return msg;
+    }
+
+    async DiasAntes5() {
         const date = new Date();
         const MesDeHoje = date.getMonth() + 1; // getMonth retorna de 0 a 11, então adicionamos 1
         const diaHoje = date.getDate(); // getDate retorna o dia do mês
@@ -66,32 +93,33 @@ class EmailController {
 
         clientes.map(async (client : any) => {
             try {
-            let msg = `${client.login.toUpperCase()} Segue a Linha Digitavel do seu Boleto da Mensalidade: \n`;
-            msg += `${client.linhadig} \n`;
+            let msg = "";
             
             const idBoleto = client.uuid_lanc; 
             
             const pix_resultados = AppDataSourcePix.getRepository(Pix);
             const pix = await pix_resultados.findOne({where: {titulo: idBoleto}});
             
-            
-            if (pix) {  // Verifica se pix e pix.qrcode existem
-                msg += "\nSegue Codigo Copia e Cola do Pix: \n\n";
-                msg += `${pix.qrcode}\n`;
-            } 
+
+            const dateString = client.datavenc;
+            const formattedDate = dateString.split(' ')[0].split('-').reverse().join('/');
 
             const pppoe = client.login;
-
+            
             const clientes = AppDataSource.getRepository(User);
 
             const email = await clientes.findOne({where: {login: pppoe}});
+            
+
+            const html_msg = this.msg(msg,formattedDate,pppoe, client.linhadig, pix?.qrcode, email?.endereco, email?.numero);
+
 
             if(email){
                 const mailOptions = {
                     from: process.env.EMAIL,
                     to: String(email.email),
-                    subject: 'Wip Telecom Boleto Mensalidade',
-                    text: msg,
+                    subject: `Wip Telecom Boleto Mensalidade ${formattedDate}`,
+                    html: html_msg,
                 };
                 // console.log(msg);
 
@@ -101,6 +129,7 @@ class EmailController {
                     await transporter.sendMail(mailOptions);         
                 } catch (error) {
                     console.log(error);
+                    this.logError(error, email.email, client);
                 }
             }
             else{
@@ -108,11 +137,124 @@ class EmailController {
             }
             } catch (error) {
                 console.log(error);
+                this.logError(error, "N/A", client);
             }
-        })
 
+            
+            
+        })
+        console.log("Finalizado");
     }
+
+    async DiasDoVencimento() {
+        const date = new Date();
+        const MesDeHoje = date.getMonth() + 1; // getMonth retorna de 0 a 11, então adicionamos 1
+        const diaHoje = date.getDate(); // getDate retorna o dia do mês
+        const diaVencimento = diaHoje;
+        
+
+        console.log(MesDeHoje);
+        console.log(diaHoje);
+        
+
+        const resultados = AppDataSourceBoleto.getRepository(Record);
+        const clientes = await resultados.find({
+            //Alias representa o resultado da data da coluna
+            where: {
+                datavenc: Raw(alias => `(MONTH(${alias}) = ${MesDeHoje} AND DAY(${alias}) = ${diaVencimento})`),
+                datadel: Raw(alias => `${alias} IS NULL`),
+                status: Raw(alias => `${alias} != 'pago'`)
+            }
+        });
+        // console.log(clientes);
+
+        clientes.map(async (client : any) => {
+            try {
+                let msg = "";
+
+            
+            const idBoleto = client.uuid_lanc; 
+            
+            const pix_resultados = AppDataSourcePix.getRepository(Pix);
+            const pix = await pix_resultados.findOne({where: {titulo: idBoleto}});
+            
+
+            const dateString = client.datavenc;
+            const formattedDate = dateString.split(' ')[0].split('-').reverse().join('/');
+
+            const pppoe = client.login;
+            
+            const clientes = AppDataSource.getRepository(User);
+
+            const email = await clientes.findOne({where: {login: pppoe}});
+
+            const html_msg = this.msg(msg,formattedDate,pppoe, client.linhadig, pix?.qrcode, email?.endereco, email?.numero);
+
+            if(email){
+                const mailOptions = {
+                    from: process.env.EMAIL,
+                    to: String(email.email),
+                    subject: `Sua Fatura Vence Hoje ${pppoe.toUpperCase()}`,
+                    html: html_msg,
+                };
+                // console.log(msg);
+
+                console.log(mailOptions);
+
+                try {
+                    await transporter.sendMail(mailOptions);         
+                } catch (error) {
+                    console.log(error);
+                    this.logError(error, email.email, client);
+                }
+            }
+            else{
+                console.log("Sem Email Cadastrado");
+            }
+            } catch (error) {
+                console.log(error);
+                this.logError(error, "N/A", client);
+            }
+
+            
+            
+        })
+        console.log("Finalizado");
+    }
+
+    logError(error: any, email: string, cliente: any) {
+        const logFilePath = path.join(__dirname, '../logs/EmailLogs.json');
+    
+        // Verificar se o arquivo existe e não está vazio
+        let logs = [];
+        if (fs.existsSync(logFilePath)) {
+            const logFileData = fs.readFileSync(logFilePath, 'utf8');
+            if (logFileData.trim()) {  // Verifica se o arquivo não está vazio
+                try {
+                    logs = JSON.parse(logFileData);
+                } catch (parseError) {
+                    console.error("Erro ao fazer parse do JSON:", parseError);
+                    logs = [];  // Se o JSON estiver corrompido, recomeça com um array vazio
+                }
+            }
+        }
+    
+        // Adicionar o novo log de erro
+        const newLog = {
+            email,
+            cliente: cliente.login,
+            error: error.message || error,
+            date: new Date().toISOString()
+        };
+        logs.push(newLog);
+    
+        // Salvar os logs de volta no arquivo
+        fs.writeFileSync(logFilePath, JSON.stringify(logs, null, 2));
+    }
+
 }
+
+
 
 const emailController = new EmailController();
 
--- src/routes/email.routes.ts
+++ src/routes/email.routes.ts
@@ -5,7 +5,8 @@ import EmailController from "../controllers/EmailController";
 
 const router = Router();
 
-router.post("/15DiasAntes", EmailController.DiasAntes15);
+router.post("/5DiasAntes", EmailController.DiasAntes5);
+router.post("/DiasDoVencimento", EmailController.DiasDoVencimento);
 
 
 export default router;
