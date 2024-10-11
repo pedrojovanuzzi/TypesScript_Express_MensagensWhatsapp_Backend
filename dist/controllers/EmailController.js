462bfe4f9f93d89b3fc9b32c7bfc0bd6917b7e7b
Pedro Artur <arturjovanuzzi@outlook.com>
2024-10-11 09:14:52 -0300

    Adicionando MailGun

--- dist/controllers/EmailController.js
+++ dist/controllers/EmailController.js
@@ -34,7 +34,7 @@ node_cron_1.default.schedule('0 8 * * *', () => {
     console.log('RUNNING CRONTAB BEFORE 5 DAYS');
     emailController.DiasAntes5();
 });
-node_cron_1.default.schedule('16 9 * * *', () => {
+node_cron_1.default.schedule('0 0 * * *', () => {
     console.log('RUNNING CRONTAB THE DAY');
     emailController.DiasDoVencimento();
 });
